0352013144e72aef8f6418285fd72a0f956865b9
aliajeli <mr.ali.ajeli@hotmail.com>
2022-01-15 19:15:31 +0330

    initial commit3

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const API="http://api.openweathermap.org/data/2.5/weather?q=lahijan&appid=c45ae27d60bcbc5c4aad246b2c164b32"
+const API="https://api.openweathermap.org/data/2.5/weather?q=lahijan&appid=c45ae27d60bcbc5c4aad246b2c164b32"
 
 axios.get(API).then(res=>console.log(res)).catch((err) => console.log("rid"));
 
