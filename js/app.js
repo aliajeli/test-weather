ab5506480416045e3d492009b24d2441ba7fb264
aliajeli <mr.ali.ajeli@hotmail.com>
2022-01-15 18:47:47 +0330

    initial commit2

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
 const API="http://dataservice.accuweather.com/locations/v1/cities/search?apikey=g2a8hjrXrnSrxOGYxg2yNSQ0vYqegS8Y&q=lahijan HTTP/1.1"
 
 axios.get(API).then(res=>console.log(res)).catch((err) => console.log(err));
+
+
+console.log("ssss")
